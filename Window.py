b3faf50304412331920f97cea446ead75f17502e
Luskasu <138623556+Luskasu@users.noreply.github.com>
2024-03-07 07:33:25 -0300

    Add files via upload

--- Window.py
+++ Window.py
@@ -1,28 +1,43 @@
 import customtkinter
+from os import listdir, path
 
 class Window:
-    def __init__(self) -> None:
+    def __init__(self):
         customtkinter.set_appearance_mode('dark')
-        self.root = customtkinter.CTk('#FFF2AB')
-        self.root.geometry("600x400")
-        self.root.title('PyNotes')
-        self.root.iconbitmap('Assets\\Icon.ico')        
+        root = customtkinter.CTk('#FFF2AB')
+        root.geometry("600x400+200+200")
+        root.title('PyNotes')
+        root.iconbitmap('Assets\\Icon.ico')   
+        root.grid_rowconfigure(0, weight=1)
+        root.grid_columnconfigure(0, weight=1)
 
-        #buttons
-        button = customtkinter.CTkButton(self.root, width=50, height=50, text='+')
-        button.pack(anchor ='nw')
-
-        #creating the entry
-        entry = customtkinter.CTkTextbox(self.root, width=600, height=400, fg_color='#FFF2AB', text_color= 'black')
-        entry.pack(anchor = 'n', fill = 'both', expand = True)
-
+        #UPPER BAR
+        self.upperBar = customtkinter.CTkFrame(root, width = 35, height = 35, fg_color='#FFF2AB', corner_radius=0)
+        self.upperBar.pack(anchor='n', fill = 'x')
         
+        #BOTTOM BAR
+        bottomBar = customtkinter.CTkFrame(root, width = 35, height = 35, fg_color='#FFF2AB', corner_radius=0)
+        bottomBar.pack(anchor='s', fill = 'x', side = 'bottom')
 
+        #ADD
+        AddButton = customtkinter.CTkButton(self.upperBar, width = 35, height = 35, fg_color='#FFF2AB', text_color='black', text='+', font = ('verdana', 21), hover_color='#EEE2A0', corner_radius=0)
+        AddButton.pack(anchor ='nw', side = 'left')
+
+        #main screen
+        self.selection = customtkinter.CTkFrame(root, width=600, height=400, fg_color='#FFF7D1', corner_radius=0)
+        self.selection.pack(anchor = 'n', fill = 'both', expand = True)
+
+        self.showEntries('entries\\')
+        root.mainloop()
         
         
-
-
-        self.root.mainloop()
+    def showEntries(self, path:str):
+        entries = list()
+        for i, entry in enumerate(listdir(path)):
+            entries.append(customtkinter.CTkButton(self.selection, height = 60, fg_color='#FFF7D1', text_color= 'black', text=entry,font = ('verdana', 12), hover_color='#F2EAC4', corner_radius=0))
+            #entries[i] = customtkinter.CTkButton(self.selection,width = 50, height = 50, fg_color='white', text=entry)
+            entries[i].pack(anchor='w', fill = 'x')
+            
         
 
 w = Window()
